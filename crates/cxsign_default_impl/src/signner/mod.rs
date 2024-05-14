54c4b4e807fad41dce09c9f69244f14803c43891
worksoup <2055038075@qq.com>
2024-05-14 11:07:16 +0800

    优化LocationInfoGetterTrait

--- crates/cxsign_default_impl/src/signner/mod.rs
+++ crates/cxsign_default_impl/src/signner/mod.rs
@@ -8,6 +8,7 @@ pub use impls::*;
 use utils::location_str_to_location;
 
 pub trait LocationInfoGetterTrait {
+    fn get_one_stored_location(&self, sign: &LocationSign) -> Option<Location>;
     fn get_locations(&self, sign: &LocationSign, location_str: &Option<String>) -> Location;
 }
 
@@ -24,6 +25,13 @@ impl<'a> From<&'a DataBase> for DefaultLocationInfoGetter<'a> {
 }
 
 impl LocationInfoGetterTrait for DefaultLocationInfoGetter<'_> {
+    fn get_one_stored_location(&self, sign: &LocationSign) -> Option<Location> {
+        let table = LocationTable::from_ref(self.0);
+        table
+            .get_location_list_by_course(sign.as_inner().course.get_id())
+            .pop()
+            .or_else(|| table.get_location_list_by_course(-1).pop())
+    }
     fn get_locations(&self, sign: &LocationSign, location_str: &Option<String>) -> Location {
         match location_str_to_location(self.0, location_str) {
             Ok(location) => location,
@@ -35,18 +43,8 @@ impl LocationInfoGetterTrait for DefaultLocationInfoGetter<'_> {
                 } else if let Some(location) = sign.get_preset_location(None) {
                     return location;
                 }
-
-                let table = LocationTable::from_ref(self.0);
-                if let Some(location) = table
-                    .get_location_list_by_course(sign.as_inner().course.get_id())
-                    .first()
-                {
-                    location.clone()
-                } else if let Some(location) = table.get_location_list_by_course(-1).first() {
-                    location.clone()
-                } else {
-                    Location::get_none_location()
-                }
+                self.get_one_stored_location(sign)
+                    .unwrap_or_else(Location::get_none_location)
             }
         }
     }
