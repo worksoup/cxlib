48e0fb7d777b12a1a61d6337d73863a35798ba5d
汤意工坊 <78455197+worksoup@users.noreply.github.com>
2024-11-08 21:47:47 +0800

    add fn `default_impl::init_all`.

--- crates/cxsign_default_impl/src/lib.rs
+++ crates/cxsign_default_impl/src/lib.rs
@@ -6,3 +6,8 @@ pub mod sign;
 pub mod signner;
 pub mod store;
 pub mod utils;
+
+/// 初始化所有需要初始化的默认实现。
+pub fn init_all() -> Result<(), cxsign_error::Error> {
+    cx_protocol::DefaultCXProtocol::init()
+}
