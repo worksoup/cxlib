aa83aea48354383930f58d58bffe28b0e3eac30e
汤意工坊 <78455197+worksoup@users.noreply.github.com>
2024-11-11 13:47:59 +0800

    improve cxsign_imageproc.

--- crates/cxsign_imageproc/src/utils.rs
+++ crates/cxsign_imageproc/src/utils.rs
@@ -1,14 +1,14 @@
-use image::buffer::ConvertBuffer;
+use crate::map::{map_colors, map_colors2};
 use image::{
-    DynamicImage, GenericImage, GenericImageView, GrayImage, Luma, LumaA, Pixel, Primitive, Rgba,
-    SubImage,
+    buffer::ConvertBuffer, DynamicImage, GenericImage, GenericImageView, GrayImage, ImageBuffer,
+    Luma, LumaA, Pixel, Primitive, Rgba, SubImage,
 };
-use imageproc::definitions::Image;
-use imageproc::map::{map_colors, map_colors2};
 pub use imageproc::point::Point;
 use num_traits::ToPrimitive;
 use std::ops::Add;
+use imageproc::contours::find_contours;
 
+pub type Image<P> = ImageBuffer<P, Vec<<P as Pixel>::Subpixel>>;
 pub fn get_rect_contains_vertex<T: Primitive, V: Iterator<Item = Point<T>>>(
     vertex: V,
 ) -> (Point<T>, Point<T>) {
@@ -52,33 +52,102 @@ pub fn cut_picture<I: GenericImageView>(
 }
 
 pub fn find_contour_rects<T: Primitive + Eq>(img: &GrayImage) -> Vec<(Point<T>, Point<T>)> {
-    let contours = imageproc::contours::find_contours::<T>(img);
+    let contours = find_contours::<T>(img);
     contours
         .into_iter()
         .map(|c| get_rect_contains_vertex(c.points.into_iter()))
         .collect()
 }
+pub mod map {
+    use image::{GenericImage, GenericImageView, ImageBuffer, Pixel};
+    use imageproc::definitions::Image;
+    pub use imageproc::point::Point;
+    pub fn map_colors<I, P, Q, F>(image: &I, f: F) -> Image<Q>
+    where
+        I: GenericImageView<Pixel = P>,
+        P: Pixel,
+        Q: Pixel,
+        F: Fn(P) -> Q,
+    {
+        let (width, height) = image.dimensions();
+        let mut out: ImageBuffer<Q, Vec<Q::Subpixel>> = ImageBuffer::new(width, height);
 
+        for y in 0..height {
+            for x in 0..width {
+                unsafe {
+                    let pix = image.unsafe_get_pixel(x, y);
+                    out.unsafe_put_pixel(x, y, f(pix));
+                }
+            }
+        }
+
+        out
+    }
+    pub fn map_colors2<I, J, P, Q, R, F>(image1: &I, image2: &J, f: F) -> Image<R>
+    where
+        I: GenericImageView<Pixel = P>,
+        J: GenericImageView<Pixel = Q>,
+        P: Pixel,
+        Q: Pixel,
+        R: Pixel,
+        F: Fn(P, Q) -> R,
+    {
+        assert_eq!(image1.dimensions(), image2.dimensions());
+
+        let (width, height) = image1.dimensions();
+        let mut out: ImageBuffer<R, Vec<R::Subpixel>> = ImageBuffer::new(width, height);
+
+        for y in 0..height {
+            for x in 0..width {
+                unsafe {
+                    let p = image1.unsafe_get_pixel(x, y);
+                    let q = image2.unsafe_get_pixel(x, y);
+                    out.unsafe_put_pixel(x, y, f(p, q));
+                }
+            }
+        }
+
+        out
+    }
+}
 pub fn image_sum<Pixel: image::Pixel, Image: GenericImageView<Pixel = Pixel>>(
     image: &Image,
-) -> Vec<f64> {
-    let zero = vec![0_f64; Pixel::CHANNEL_COUNT as usize];
-    let sum = image.pixels().fold(zero, |mut acc, p| {
-        let channels = p.2.channels();
+    mask: &[bool],
+) -> (Vec<f64>, usize) {
+    fn add<P: image::Pixel>(
+        (mut acc, count): (Vec<f64>, usize),
+        p: (usize, (u32, u32, P)),
+    ) -> (Vec<f64>, usize) {
+        let channels = p.1 .2.channels();
         for (index, acc) in acc.iter_mut().enumerate() {
             *acc = acc.add(channels[index].to_f64().expect("Can't convert to f64"));
         }
-        acc
-    });
+        (acc, count + 1)
+    }
+    let zero = vec![0_f64; Pixel::CHANNEL_COUNT as usize];
+    let sum =
+        if mask.len() < (image.width() * image.width()) as usize {
+            image
+                .pixels()
+                .enumerate()
+                .fold((zero, 0_usize), |acc, p| add(acc, p))
+        } else {
+            image.pixels().enumerate().fold((zero, 0_usize), |acc, p| {
+                if mask[p.0] {
+                    add(acc, p)
+                } else {
+                    acc
+                }
+            })
+        };
     sum
 }
 pub fn image_mean<Pixel: image::Pixel, Image: GenericImageView<Pixel = Pixel>>(
     image: &Image,
+    mask: &[bool],
 ) -> Vec<f64> {
-    let sum = image_sum(image);
-    let (w, h) = image.dimensions();
-    let size = (w * h) as f64;
-    sum.into_iter().map(|sum| sum / size).collect()
+    let (sum, count) = image_sum(image, mask);
+    sum.into_iter().map(|sum| sum / count as f64).collect()
 }
 
 pub fn rgb_alpha_channel<I, C>(image: &I) -> Image<Luma<C>>
@@ -121,26 +190,36 @@ pub fn download_image(
     let img = image_from_bytes(v);
     Ok(img)
 }
-pub fn find_max_ncc(big_img: &GrayImage, small_image: &GrayImage) -> u32 {
-    let mean = image_mean(small_image);
-    let small_image = map_colors(small_image, |p| Luma([p[0] as f64 - mean[0]]));
+pub fn find_max_ncc(
+    big_image: SubImage<&DynamicImage>,
+    small_image: SubImage<&DynamicImage>,
+) -> u32 {
+    let mask = rgb_alpha_channel(&small_image.to_image());
+    let mask_mean = *image_mean(&mask, &[]).last().expect("No image mean");
+    let mask = mask
+        .iter()
+        .map(|p| (*p as f64) >= mask_mean)
+        .collect::<Vec<_>>();
+    let mean = image_mean(&*small_image, &mask);
+    let small_image: GrayImage = small_image.to_image().convert();
+    let small_image = map_colors(&small_image, |p| Luma([p[0] as f64 - mean[0]]));
     let mut max_ncc = 0.0;
     let mut max_x = 0;
-    for x in 0..big_img.width() - small_image.width() {
-        let window = crate::cut_picture(
-            big_img,
+    let big_image: GrayImage = big_image.to_image().convert();
+    for x in 0..big_image.width() - small_image.width() {
+        let window = cut_picture(
+            &big_image,
             Point { x, y: 0 },
             Point {
                 x: small_image.width(),
                 y: small_image.height(),
             },
-        )
-        .to_image();
-        let window_mean = image_mean(&window);
-        let window = map_colors(&window, |p| Luma([p[0] as f64 - window_mean[0]]));
+        );
+        let window_mean = image_mean(&*window, &mask);
+        let window = map_colors(&*window, |p| Luma([p[0] as f64 - window_mean[0]]));
         let a = map_colors2(&window, &small_image, |w, t| Luma([w[0] * t[0]]));
         let b = map_colors(&window, |w| Luma([w[0] * w[0]]));
-        let ncc = image_sum(&a)[0] / image_sum(&b)[0];
+        let ncc = image_sum(&a, &mask).0[0] / image_sum(&b, &mask).0[0];
         if ncc > max_ncc {
             max_x = x;
             max_ncc = ncc;
@@ -148,16 +227,15 @@ pub fn find_max_ncc(big_img: &GrayImage, small_image: &GrayImage) -> u32 {
     }
     max_x
 }
-pub fn find_sub_image<F: Fn(&GrayImage, &GrayImage) -> u32>(
+pub fn find_sub_image<F: Fn(SubImage<&DynamicImage>, SubImage<&DynamicImage>) -> u32>(
     big_image: &DynamicImage,
     small_image: &DynamicImage,
     a: F,
 ) -> u32 {
     let small_image_alpha = rgb_alpha_channel(small_image);
-    let rects = crate::find_contour_rects::<u32>(&small_image_alpha);
+    let rects = find_contour_rects::<u32>(&small_image_alpha);
     let (lt, rb) = rects[0];
-    let small_image = cut_picture(small_image, lt, rb - lt).to_image();
-    let small_image = small_image.convert();
+    let small_image = cut_picture(small_image, lt, rb - lt);
     let small_w = small_image.width();
     let big_w = big_image.width();
     let big_img = cut_picture(
@@ -168,6 +246,5 @@ pub fn find_sub_image<F: Fn(&GrayImage, &GrayImage) -> u32>(
             y: 0,
         } + (rb - lt),
     );
-    let big_img = big_img.to_image().convert();
-    a(&big_img, &small_image)
+    a(big_img, small_image)
 }
