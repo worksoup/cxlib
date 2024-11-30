7045f9cd02f340e34f545c85dd13c156638938e6
Learturely <learturely@gmail.com>
2024-11-30 15:16:14 +0800

    fixup target features. 现在应当能编译至 android 平台。

--- crates/cxlib_default_impl/src/signner/utils.rs
+++ crates/cxlib_default_impl/src/signner/utils.rs
@@ -1,16 +1,40 @@
-use crate::sign::QrCodeSign;
 use cxlib_error::Error;
+use cxlib_qrcode_utils::find_qrcode_sign_enc_in_url;
 use cxlib_utils::*;
 use std::path::PathBuf;
-use cxlib_qrcode_utils::find_qrcode_sign_enc_in_url;
 
+#[cfg(not(any(target_os = "linux", target_os = "windows", target_os = "macos")))]
+pub fn enc_gen(path: &Option<PathBuf>, enc: &Option<String>) -> Result<String, Error> {
+    let enc = if let Some(enc) = enc {
+        enc.clone()
+    } else if let Some(pic) = path {
+        if std::fs::metadata(pic).unwrap().is_dir() {
+            if let Some(pic) = crate::utils::pic_dir_or_path_to_pic_path(pic)?
+                && let Some(enc) = crate::utils::pic_path_to_qrcode_result(pic.to_str().unwrap())
+            {
+                enc
+            } else {
+                return Err(Error::EncError(
+                    "图片文件夹下没有图片（`png` 或 `jpg` 文件）！".to_owned(),
+                ));
+            }
+        } else if let Some(enc) = crate::utils::pic_path_to_qrcode_result(pic.to_str().unwrap()) {
+            enc
+        } else {
+            return Err(Error::EncError("二维码中没有 `enc` 参数！".to_owned()));
+        }
+    } else {
+        return Err(Error::EncError("未获取到 `enc` 参数！".to_owned()));
+    };
+    Ok(enc)
+}
+#[cfg(any(target_os = "linux", target_os = "windows", target_os = "macos"))]
 pub fn enc_gen(
-    sign: &QrCodeSign,
+    sign: &crate::sign::QrCodeSign,
     path: &Option<PathBuf>,
     enc: &Option<String>,
-    #[cfg(any(target_os = "linux", target_os = "windows", target_os = "macos"))] precisely: bool,
+    precisely: bool,
 ) -> Result<String, Error> {
-    #[cfg(any(target_os = "linux", target_os = "windows", target_os = "macos"))]
     let enc = if let Some(enc) = enc {
         enc.clone()
     } else if let Some(pic) = path {
@@ -35,29 +59,6 @@ pub fn enc_gen(
         enc
     } else {
         return Err(Error::EncError("截屏时未获取到 `enc` 参数！".to_owned()));
-    };
-
-    #[cfg(not(any(target_os = "linux", target_os = "windows", target_os = "macos")))]
-    let enc = if let Some(enc) = enc {
-        enc.clone()
-    } else if let Some(pic) = path {
-        if std::fs::metadata(pic).unwrap().is_dir() {
-            if let Some(pic) = crate::utils::pic_dir_or_path_to_pic_path(pic)?
-                && let Some(enc) = crate::utils::pic_path_to_qrcode_result(pic.to_str().unwrap())
-            {
-                enc
-            } else {
-                return Err(Error::EncError(
-                    "图片文件夹下没有图片（`png` 或 `jpg` 文件）！".to_owned(),
-                ));
-            }
-        } else if let Some(enc) = crate::utils::pic_path_to_qrcode_result(pic.to_str().unwrap()) {
-            enc
-        } else {
-            return Err(Error::EncError("二维码中没有 `enc` 参数！".to_owned()));
-        }
-    } else {
-        return Err(Error::EncError("未获取到 `enc` 参数！".to_owned()));
     };
     Ok(enc)
 }
