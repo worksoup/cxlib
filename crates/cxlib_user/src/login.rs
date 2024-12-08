a6f269ee7d012228e500ffb5022f8646eed4679e
汤意工坊 <78455197+worksoup@users.noreply.github.com>
2024-12-08 16:43:11 +0800

    优化项目结构。

--- crates/cxlib_user/src/login.rs
+++ crates/cxlib_user/src/login.rs
@@ -1,6 +1,7 @@
 use crate::protocol;
 use cxlib_error::LoginError;
 use cxlib_protocol::ProtocolItem;
+use cxlib_utils::pkcs7_pad;
 use log::{trace, warn};
 use onceinit::{OnceInit, OnceInitState, StaticDefault};
 use std::{
@@ -37,25 +38,6 @@ impl DefaultLoginSolver {
         }
         Ok(name.to_owned())
     }
-    pub fn pkcs7_pad<const BLOCK_SIZE: usize>(data: &[u8]) -> Vec<[u8; BLOCK_SIZE]> {
-        let len = data.len();
-        let batch = len / BLOCK_SIZE;
-        let m = len % BLOCK_SIZE;
-        let len2 = BLOCK_SIZE - m;
-        let mut r = vec![[0u8; BLOCK_SIZE]; batch + 1];
-        let pad_num = ((BLOCK_SIZE - m) % 0xFF) as u8;
-        let r_data = r.as_mut_ptr() as *mut u8;
-        unsafe {
-            std::ptr::copy_nonoverlapping(data.as_ptr(), r_data, len);
-            std::ptr::copy_nonoverlapping(
-                vec![pad_num; len2].as_ptr(),
-                r_data.add(batch * BLOCK_SIZE + m),
-                len2,
-            );
-        }
-        r
-    }
-
     pub fn des_enc(data: &[u8], key: [u8; 8]) -> String {
         use des::{
             cipher::{generic_array::GenericArray, BlockEncrypt as _, KeyInit as _},
@@ -64,7 +46,7 @@ impl DefaultLoginSolver {
         let key = GenericArray::from(key);
         let des = Des::new(&key);
         let mut data_block_enc = Vec::new();
-        for block in Self::pkcs7_pad(data) {
+        for block in pkcs7_pad(data) {
             let mut block = GenericArray::from(block);
             des.encrypt_block(&mut block);
             let mut block = block.to_vec();
--- crates/cxlib_utils/src/lib.rs
+++ crates/cxlib_utils/src/lib.rs
@@ -1,7 +1,44 @@
-mod debug;
-mod interact;
-mod ureq;
-
+pub use crypto::*;
 pub use debug::*;
 pub use interact::*;
 pub use ureq::*;
+mod debug;
+mod interact;
+mod ureq {
+    pub fn ureq_get_bytes(
+        agent: &ureq::Agent,
+        url: &str,
+        referer: &str,
+    ) -> Result<Vec<u8>, Box<ureq::Error>> {
+        let mut img = Vec::new();
+        agent
+            .get(url)
+            .set("Referer", referer)
+            .call()?
+            .into_reader()
+            .read_to_end(&mut img)
+            .unwrap();
+        Ok(img)
+    }
+}
+
+mod crypto {
+    pub fn pkcs7_pad<const BLOCK_SIZE: usize>(data: &[u8]) -> Vec<[u8; BLOCK_SIZE]> {
+        let len = data.len();
+        let batch = len / BLOCK_SIZE;
+        let m = len % BLOCK_SIZE;
+        let len2 = BLOCK_SIZE - m;
+        let mut r = vec![[0u8; BLOCK_SIZE]; batch + 1];
+        let pad_num = ((BLOCK_SIZE - m) % 0xFF) as u8;
+        let r_data = r.as_mut_ptr() as *mut u8;
+        unsafe {
+            std::ptr::copy_nonoverlapping(data.as_ptr(), r_data, len);
+            std::ptr::copy_nonoverlapping(
+                vec![pad_num; len2].as_ptr(),
+                r_data.add(batch * BLOCK_SIZE + m),
+                len2,
+            );
+        }
+        r
+    }
+}
