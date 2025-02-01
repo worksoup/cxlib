a4dd7ff9d0ef30518a00aad05f18121d64f01470
汤意工坊 <78455197+worksoup@users.noreply.github.com>
2025-02-01 13:15:52 +0800

    cxlib_app

---
+++ crates/cxlib_app/src/default_impl/cmd_app/accounts.rs
@@ -0,0 +1,72 @@
+use crate::{AppTrait, CmdApp, CmdMetaAppTrait};
+use clap::{arg, ArgMatches, Command, CommandFactory, FromArgMatches, Parser};
+use cxlib_internal::{
+    default_impl::store::{AccountTable, DataBase},
+    user::{LoginSolverWrapper, Session},
+};
+
+#[derive(Parser, Debug, Clone)]
+#[command(name = "accounts")]
+#[clap(about = "列出所有账号。")]
+pub struct AccountsParser {
+    /// 重新获取账号信息并缓存。
+    #[arg(short, long)]
+    fresh: bool,
+}
+pub struct AccountsCmdApp {
+    command: Command,
+}
+
+impl AccountsCmdApp {
+    pub fn new() -> Self {
+        let command = AccountsParser::command();
+        Self { command }
+    }
+}
+impl Default for AccountsCmdApp {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+impl<Context: AsRef<DataBase>> AppTrait<Context> for AccountsCmdApp {
+    type OwnedData = AccountsParser;
+
+    fn run(&self, db: &Context, AccountsParser { fresh }: Self::OwnedData) {
+        let sessions: Vec<Session> = if fresh {
+            AccountTable::get_accounts(db.as_ref())
+                .into_iter()
+                .filter_map(|a| {
+                    let session = Session::relogin(
+                        a.uname(),
+                        a.enc_pwd(),
+                        &LoginSolverWrapper::new(a.login_type()),
+                    );
+                    session.ok()
+                })
+                .collect()
+        } else {
+            // 列出所有账号。
+            AccountTable::get_sessions(db.as_ref())
+                .into_values()
+                .collect()
+        };
+        for session in sessions {
+            println!(
+                "{}, {}, {}",
+                session.get_uname(),
+                session.get_stu_name(),
+                session.get_uid()
+            );
+        }
+    }
+}
+impl<Context: AsRef<DataBase> + 'static> CmdMetaAppTrait<CmdApp<Context>, Context>
+    for AccountsCmdApp
+{
+    fn subcommand(&self) -> Option<&Command> {
+        Some(&self.command)
+    }
+    fn read_owned_data(&self, _: &Context, matches: &[&ArgMatches]) -> AccountsParser {
+        AccountsParser::from_arg_matches(matches.last().unwrap()).unwrap()
+    }
+}
---
+++ crates/cxlib_app/src/default_impl/cmd_app/course.rs
@@ -0,0 +1,64 @@
+use crate::{AppTrait, CmdApp, CmdMetaAppTrait};
+use clap::{ArgMatches, Command, CommandFactory, FromArgMatches, Parser};
+use cxlib_internal::{
+    default_impl::store::{AccountTable, DataBase},
+    types::Course,
+};
+#[derive(Parser, Debug, Clone)]
+#[command(name = "courses")]
+#[clap(about = "获取课程信息。")]
+pub struct CoursesParser {
+    /// 待操作账号，格式为以半角逗号隔开的 uid (可通过 accounts 子命令查看).
+    #[arg(short, long)]
+    uid: Option<String>,
+}
+pub struct CoursesCmdApp {
+    command: Command,
+}
+impl CoursesCmdApp {
+    pub fn new() -> Self {
+        Self {
+            command: CoursesParser::command(),
+        }
+    }
+}
+impl Default for CoursesCmdApp {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+impl<Context: AsRef<DataBase>> AppTrait<Context> for CoursesCmdApp {
+    type OwnedData = CoursesParser;
+
+    fn run(&self, db: &Context, data: CoursesParser) {
+        let (sessions, _) = if let Some(uid_list_str) = &data.uid {
+            (
+                AccountTable::get_sessions_by_uid_list_str(db.as_ref(), uid_list_str),
+                true,
+            )
+        } else {
+            (AccountTable::get_sessions(db.as_ref()), false)
+        };
+        // 获取课程信息。
+        let courses = Course::get_courses(sessions.values()).unwrap_or_default();
+        // 列出所有课程。
+        for (c, _) in courses {
+            println!("{}", c);
+        }
+    }
+}
+impl<Context: AsRef<DataBase> + 'static> CmdMetaAppTrait<CmdApp<Context>, Context>
+    for CoursesCmdApp
+{
+    fn subcommand(&self) -> Option<&Command> {
+        Some(&self.command)
+    }
+
+    fn read_owned_data(
+        &self,
+        _: &Context,
+        matches: &[&ArgMatches],
+    ) -> <Self as AppTrait<Context, ()>>::OwnedData {
+        CoursesParser::from_arg_matches(matches.last().unwrap()).unwrap()
+    }
+}
